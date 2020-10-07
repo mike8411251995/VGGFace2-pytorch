91d44e3b73c6f3bb0337db3ef8477286e211dda4
Mike Lee <mike8411251995@yahoo.com.tw>
2020-10-07 18:02:17 +0800

    added extraction for test images

--- extract.sh
+++ extract.sh
@@ -1,1 +1,3 @@
 python demo.py extract --arch_type resnet50_ft --weight_file ./pretrained/resnet50_ft_weight.pkl --dataset_dir ../PCNet/dataset/vggface2/ --feature_dir ~/data/vggface2/extracted/resnet50_ft/ --test_img_list_file ../PCNet/dataset/vggface2/train_list.txt --log_file ~/data/vggface2/extracted/resnet50_ft/log/train.txt --meta_file ../PCNet/dataset/vggface2/identity_meta.csv
+
+python demo.py extract --arch_type resnet50_ft --weight_file ./pretrained/resnet50_ft_weight.pkl --dataset_dir ../PCNet/dataset/vggface2/ --feature_dir ~/data/vggface2/extracted/resnet50_ft/ --test_img_list_file ../PCNet/dataset/vggface2/test_list.txt --log_file ~/data/vggface2/extracted/resnet50_ft/log/test.txt --meta_file ../PCNet/dataset/vggface2/identity_meta.csv
